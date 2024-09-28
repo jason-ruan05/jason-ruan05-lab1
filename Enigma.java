4ce3ed742f41e5f5225f3128e3ba7b2fe4af1b23
Jason Ruan <jason.ruan@gwmail.gwu.edu>
2024-09-28 14:11:05 -0400

    final solution

--- Enigma.java
+++ Enigma.java
@@ -1,14 +1,14 @@
 public class Enigma{
-
+    //The Rotor Strings
     private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
         "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
         "#TGOWHLIFMCSZYRVXQABUPEJKND"};
 
-
+    //Assignment of an array with rotors
     private Rotor rotors[];
-        
+    //Engima constructor
     public Enigma(int id1, int id2, int id3, String start){
 
         rotors = new Rotor[3];
@@ -18,56 +18,70 @@ public class Enigma{
         
     }
 
-
-    public String decrypt(String message){        
+    //This methow would decrypt a message on a engima machine
+    //@param an already encrypted message
+    //@return the method would return the original message before it was encrypted
+    public String decrypt(String message){   
+        //Assign a new variable which is the message we want to decrypt but in char[]     
         char[] messageTemp = message.toCharArray();
+        //Creates a new char[] in which we would store each decrypted letter to get
+        //pur message
         char[] messageFinal = new char[message.length()];
+        //Assign the three rotors 
         Rotor inner = rotors[0];
         Rotor middle = rotors[1];
         Rotor outer = rotors[2];
+        //This would itterate through each char in the message we want to decrypt
         for(int i = 0; i < message.length(); i++){
+            //The decryption Algo which goes from
+            //outer->middle->outer->inner
             int step1 = outer.indexOf(messageTemp[i]);
             char step2 = middle.charAt(step1);
             int step3 = outer.indexOf(step2);
             char step4 = inner.charAt(step3);
+            //assign the decrypted char into the final char[] which would be the output
             messageFinal[i] = step4;
+            //rotate the rotors
             rotate();            
         }
-        // for(int i = 0; i <= 27; i++){
-        //     int step1 = outer.indexOf(messageTemp[i]);
-        //     char step2 = middle.charAt(step1);
-        //     int step3 = outer.indexOf(step2);
-        //     char step4 = inner.charAt(step3);
-        //     messageFinal[j] = step4;
-        //     rotate();
-        //     break;
-        // }
+        //Turn our final decrypted char[] into a string and return it
         String output = new String(messageFinal);
         return output;
     }
 
 
-    
+    //This method would encrypt a message in our engima machine
+    //@param the paramteres is the message we want to encrypt
+    //@return this methof would return the ecrypted message
     public String encrypt(String message){
+        //Assign a new variable which is the message we want to encrypt but in char[]     
         char[] messageTemp = message.toCharArray();
+        // Creates an empty char[] which we would sotre each enrytped char
         char[] messageFinal = new char[message.length()];
+        //Assign all the rotors 
         Rotor inner = rotors[0];
         Rotor middle = rotors[1];
         Rotor outer = rotors[2];
+        //itterate through each char in the message to encrypt
         for(int i = 0; i < message.length() ; i++){
+            //The encryption algo
+            //inner->outer->middle->outer
             int step1 = inner.indexOf(messageTemp[i]);
             char step2 = outer.charAt(step1);
             int step3 = middle.indexOf(step2);
             char step4 = outer.charAt(step3);
+            //Assign the encrypted char into the final char[]
             messageFinal[i] = step4;
+            //rotate the rotors
             rotate();
         }
+        // Turn the final encrypted char[] into a string and return it
         String output = new String(messageFinal);
         return output;
         }
     
 
-    
+    // This method is used to rotate the rotors from inner to middle to outer
     private void rotate(){
         if(rotors[0].rotate()){
             if(rotors[1].rotate()){
--- Rotor.java
+++ Rotor.java
@@ -1,10 +1,10 @@
 public class Rotor {
 
-    
+    // Assignment of variables
     private String rotorValues;
     private char startChar;
     public int length;
-        
+    // The constructor  
     public Rotor(String v, char c){
         this.rotorValues = new String(v);
         this.startChar = c;
@@ -12,33 +12,54 @@ public class Rotor {
         while(!this.rotate()); 
             
     }
-    
+    //This method would rotate a rotor one click clockwise
+    //@return this would return true if current char matches with starting
+    //char and false otherwise
     public boolean rotate(){
-        char[] rotorTemp = rotorValues.toCharArray();
-        startChar = rotorTemp[0];
-        for(int i = 0; i < rotorTemp.length - 1; i++){
-            rotorTemp[i] = rotorTemp[i + 1];
+        // This would create a new variable with a char[] of the rotor
+        char[] rotorTemp = this.rotorValues.toCharArray();
+        // Store the last char in order to put it at the top of the rotor
+        char lastChar = rotorTemp[rotorTemp.length - 1];
+        // An itteration loop in order to shift all elements in the array right one spot
+        for(int i = rotorTemp.length - 1; i > 0; i--){
+            rotorTemp[i] = rotorTemp[i - 1];
         }
-        rotorTemp[rotorTemp.length - 1] = startChar;
-        String rotorFinal = new String(rotorTemp);
-        rotorValues = rotorFinal;
-        return true;
-        
+        // Assign the first element in the array to the lastchar previously because 
+        // each last char in the rotor belongs up top
+        rotorTemp[0] = lastChar;
+        // Turn the temp variable from a char[] to a string an assign it back to our constructor thus
+        // completeing a rotate
+        this.rotorValues = new String(rotorTemp);
+        // The rotate method would return true if the current char matches it's starting char
+        if(this.rotorValues.charAt(0) == startChar){
+            return true;
+        }
+        //false otherwise
+        return false;
     }
     
-
+    // This method would find the index of a specific char in the rotor
+    //@param The parameter is the char we are looking for
+    //@return returns the index of a the char we are looking for
     public int indexOf(char c){
+        //Create a new variable of the rotor into a char[]
         char[] rotorTemp = rotorValues.toCharArray();
+        //itterate through each element in the char array
         for(int i = 0 ; i < rotorTemp.length; i++){
+            //If the char matches with the char called in the param return the i
             if(rotorTemp[i] == c){
                 return i;
             }
         }
+        //returns -1 if not found, however it should never return this
         return -1;
     }
-
+    //Thi method would find the char at a specific index we are looking for
+    //@param the index where the char we want to get
     public char charAt(int idx){
+        //This would turn the rotor into a char[]
         char[] rotorTemp = rotorValues.toCharArray();
+        //This would return the single char from the char[] where the index/position lies
         return rotorTemp[idx];
     }
 }
